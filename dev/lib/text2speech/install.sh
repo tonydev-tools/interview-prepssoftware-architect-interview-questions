872b725499b8bd8cc593689130a5577161cb3332
tonydev-tools <153137144+tonydev-tools@users.noreply.github.com>
2024-06-19 21:52:45 +0200

    Update dev/lib/text2speech/install.sh

--- dev/lib/text2speech/install.sh
+++ dev/lib/text2speech/install.sh
@@ -11,3 +11,4 @@ sudo apt install xclip || echo -e "\n\n\t\033[34merror \033[33mxclip\033[0m not 
 sudo apt install sox || echo -e "\n\n\t\033[34merror \033[33msox\033[0m not installed\033[0m"
 
 sudo apt-get install libsox-fmt-mp3 || echo -e "\n\n\t\033[34merror libsox-fmt-mp3 not installed\033[0m"
+https://github.com/tonydev-tools/interview-prepssoftware-architect-interview-questions/blob/main/dev/lib/text2speech/install.sh
